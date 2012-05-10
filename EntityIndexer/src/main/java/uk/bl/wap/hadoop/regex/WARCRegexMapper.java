437a588578b23e2367a50e9496a732d432da55f2
Andrew Jackson <Andrew.Jackson@bl.uk>
2012-05-10 17:51:27 -0400

    Stripped back to output raw data only.

--- EntityIndexer/src/main/java/uk/bl/wap/hadoop/regex/WARCRegexMapper.java
+++ EntityIndexer/src/main/java/uk/bl/wap/hadoop/regex/WARCRegexMapper.java
@@ -4,6 +4,8 @@ import java.io.IOException;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import org.apache.commons.httpclient.URI;
+import org.apache.commons.httpclient.URIException;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapred.JobConf;
 import org.apache.hadoop.mapred.MapReduceBase;
@@ -54,6 +56,10 @@ public class WARCRegexMapper extends MapReduceBase implements Mapper<Text, Writa
 		String newKey = "0/unknown";			
 		if( !header.getHeaderFields().isEmpty() ) {
 			newKey = header.getDate() + "/" + header.getUrl();
+			// Reduce this to just the year and the host:
+			//String year = extractYear(header.getDate());
+			//String host = extractHost(header.getUrl());
+			//newKey = year + "/" + host;
 		}
 		
 		// Collect the matches:
@@ -63,4 +69,36 @@ public class WARCRegexMapper extends MapReduceBase implements Mapper<Text, Writa
 		}
 	}
 	
+	/**
+	 * 
+	 * @param timestamp
+	 * @return
+	 */
+	private static String extractYear(String timestamp) {
+		String waybackYear = "unknown-year";
+		String waybackDate = timestamp.replaceAll( "[^0-9]", "" );
+		if( waybackDate != null ) 
+			waybackYear = waybackDate.substring(0,4);
+		return waybackYear;
+	}
+	
+	/**
+	 * 
+	 * @param url
+	 * @return
+	 */
+	private static String extractHost(String url) {
+		String host = "unknown.host";
+		URI uri = null;
+		// Attempt to parse:
+		try {
+			uri = new URI(url,false);
+			// Extract domain:
+			host = uri.getHost();
+		} catch ( Exception e ) {
+			// Return a special hostname if parsing failed:
+			host = "malformed.host";
+		}
+		return host;
+	}
 }
