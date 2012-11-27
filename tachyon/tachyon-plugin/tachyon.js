abd11a7ba4bcc9b8a04f8a67115053587c2c9dff
Andrew Jackson <Andrew.Jackson@bl.uk>
2012-11-27 16:46:29 +0000

    Can now be switched on and off, has a basic icon too.

--- tachyon/tachyon-plugin/tachyon.js
+++ tachyon/tachyon-plugin/tachyon.js
@@ -1,5 +1,33 @@
+/* This is used to record the state of the plugin - active or not. */
+var listenerIsActive = false;
+
+function toggleActive(tab) {
+    if( listenerIsActive ) {
+        listenerIsActive = false;
+        chrome.browserAction.setIcon({path:"icon.png"});
+        // TODO Strip our archival prefix, redirect to live site.
+    } else {
+        listenerIsActive = true;
+        chrome.browserAction.setIcon({path:"icon-on.png"});
+        // Refresh tab to force switch to archival version:
+        chrome.tabs.reload(tab.id);
+    }
+}
+
+chrome.browserAction.onClicked.addListener(toggleActive);
+
+/**
+ * This takes the url of any request and redirects it to the TimeGate.
+ * The actual Datetime request is handled later (see below).
+ */
 chrome.webRequest.onBeforeRequest.addListener(
-  function(details){ 
+  function(details){
+    // Pass through if the plugin is inactive.
+    if( !listenerIsActive ) {
+      return {};
+    }
+
+    // Re-direct to the preferred TimeGate:
     if( ! (details.url.indexOf("http://www.webarchive.org.uk/wayback/memento/") == 0) ) {
         return { redirectUrl: "http://www.webarchive.org.uk/wayback/memento/timegate/"+(details.url.replace("?","%3F")) }
     } else {
@@ -7,23 +35,29 @@ chrome.webRequest.onBeforeRequest.addListener(
     }
   },
   {
-    urls: ["<all_urls>"]
+    urls: ["http://*/*", "https://*/*"]
   },
   ["blocking"]
 );
 
+/**
+ * This modifies the request headers, adding in the desire Datetime.
+ */
 chrome.webRequest.onBeforeSendHeaders.addListener(
     function(details) {
-        //if( details.url.indexOf("http://www.webarchive.org.uk/wayback/memento") == 0 ) {
-            details.requestHeaders.push( 
-                { name: "Accept-Datetime", 
-                  value: "Thu, 31 May 2001 20:35:00 GMT" }
-            );
-        //}
+        // Pass through if the plugin is inactive.
+        if( !listenerIsActive ) {
+          return {requestHeaders: details.requestHeaders};
+        }
+        // Push in the Accept-Datetime header:
+        details.requestHeaders.push( 
+            { name: "Accept-Datetime", 
+              value: "Thu, 31 May 2001 20:35:00 GMT" }
+        );
         return {requestHeaders: details.requestHeaders};
     },
     {
-      urls: ["<all_urls>"]
+       urls: ["http://*/*", "https://*/*"]
     },
     ['requestHeaders','blocking']
  );
