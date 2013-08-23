8eb000d6c8e956cceeae2eafc180d232aed01762
Andrew Jackson <Andrew.Jackson@bl.uk>
2013-08-23 13:55:50 +0100

    Shifted python code into a subfolder.

--- README.md
+++ README.md
@@ -2,3 +2,5 @@ shine
 =====
 
 An experimental faceted SOLR browser built using Django
+
+Uses [solrpy](http://pythonhosted.org/solrpy/reference.html)
--- python/shine_solr/views.py
+++ python/shine_solr/views.py
@@ -3,11 +3,15 @@ from django.shortcuts import render_to_response
 import datetime
 import pprint
 import solr
+import logging
+
+logging.basicConfig(level=logging.INFO)
+
 
 solr_url = "http://localhost:8080/discovery"
 #solr_url = "http://192.168.45.10:8983/solr/aadda-discovery"
 
-s = solr.Solr(solr_url)
+s = solr.Solr(solr_url, debug=True)
 s.select = solr.SearchHandler(s, "/select","__")
 
 # Create your views here.
@@ -16,8 +20,12 @@ def search(request):
   q = request.GET.get('q', "*")
 
   r = s.select(
-    q, facet='true', facet__sort='count',
+    q, sort='sentiment_score', sort_order='asc',
+    group='true', group__field='domain', group__main='true',
+    facet='true', facet__sort='count',
     facet__field=['crawl_year', 'content_type', 'content_ffb'],
     facet__mincount=1, f__crawl_year__facet__mincount=0)
 
-  return render_to_response('search_result_list.html', {'numFound': r.numFound, 'r': r, 'facets': r.facet_counts['facet_fields']})
+  print dir(r)
+
+  return render_to_response('search_result_list.html', {'numFound': r, 'r': r, 'facets': r.facet_counts['facet_fields']})
