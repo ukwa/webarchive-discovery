2c87fd1cfa1020355547cfdcac7a3e6b87e212ed
Andrew Jackson <Andrew.Jackson@bl.uk>
2013-09-27 13:21:45 +0100

    Added basic bootstrap layout.

--- shine/app/uk/bl/wa/shine/SolrShine.java
+++ shine/app/uk/bl/wa/shine/SolrShine.java
@@ -23,42 +23,54 @@ import play.Logger;
  */
 public class SolrShine {
 
-	private SolrServer solr = null;
+	private HttpSolrServer solr = null;
+	
+	private String[] facets = {
+			"content_encoding",
+			"content_ffb",
+			"content_language",
+			"domain",
+			"author",
+			"content_type",
+			"content_type_norm",
+			"crawl_year",
+			"content_type_ext",
+			"links_public_suffixes",
+			"generator",
+			"last_modified_year",
+			"content_type_full",
+			"postcode_district",
+			"sentiment",
+			"public_suffix" };
 	
 	public SolrShine(String host) {
-		 solr = new HttpSolrServer(host);		
-	}
+		 solr = new HttpSolrServer(host);
+    }
 	
 	public QueryResponse search( String query, Map<String,List<String>> params ) throws SolrServerException {
 		SolrQuery parameters = new SolrQuery();
+		// The query:
 		parameters.set("q", query);
-		parameters.addFacetField(
-				"content_encoding",
-				"content_ffb",
-				"content_language",
-				"domain",
-				"author",
-				"content_type",
-				"content_type_norm",
-				"crawl_year",
-				"content_type_ext",
-				"links_public_suffixes",
-				"generator",
-				"last_modified_year",
-				"content_type_full",
-				"postcode_district",
-				"sentiment",
-				"public_suffix");
+		// Facets:
+		for( String f : facets ) {
+			parameters.addFacetField("{!ex="+f+"}"+f);
+		}
 		parameters.setFacetMinCount(1);
 		List<String> fq = new ArrayList<String>();
 		for( String param : params.keySet() ) {
-			fq.add(param+":"+params.get(param).get(0));
+			String field = param;
+			if( field.startsWith("-")) field = field.replaceFirst("-", "");
+			fq.add("{!tag="+field+"}"+param+":"+params.get(param).get(0));
 		}
 		if( fq.size() > 0 ) {
 			parameters.setFilterQueries(fq.toArray(new String[fq.size()]));
 		}
+		// Sorts:
 		parameters.setSort("crawl_date", ORDER.asc);
-		return solr.query(parameters);
+		// Perform the query:
+		QueryResponse res = solr.query(parameters);
+		Logger.info("Q: "+res.getRequestUrl());
+		return res;
 	}
 	
 	private String temp( String query ) throws SolrServerException {
