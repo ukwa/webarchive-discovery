8325140b17cf4a080b44ba3d6ddae84e537cf750
Andrew Jackson <Andrew.Jackson@bl.uk>
2014-01-28 12:24:07 +0000

    Added sketch of Apache Preflight PDF parser.
    Using PDF/A spec. to check for preservation risks.

---
+++ warc-indexer/src/main/java/uk/bl/wa/parsers/ApachePreflightParser.java
@@ -0,0 +1,107 @@
+/**
+ * 
+ */
+package uk.bl.wa.parsers;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.activation.FileDataSource;
+
+import org.apache.pdfbox.preflight.PreflightDocument;
+import org.apache.pdfbox.preflight.ValidationResult;
+import org.apache.pdfbox.preflight.ValidationResult.ValidationError;
+import org.apache.pdfbox.preflight.exception.SyntaxValidationException;
+import org.apache.pdfbox.preflight.parser.PreflightParser;
+import org.apache.tika.exception.TikaException;
+import org.apache.tika.metadata.Metadata;
+import org.apache.tika.mime.MediaType;
+import org.apache.tika.parser.AbstractParser;
+import org.apache.tika.parser.ParseContext;
+import org.xml.sax.ContentHandler;
+import org.xml.sax.SAXException;
+
+import uk.bl.wa.util.InputStreamDataSource;
+
+/**
+ * @author Andrew Jackson <Andrew.Jackson@bl.uk>
+ *
+ */
+public class ApachePreflightParser extends AbstractParser {
+
+    /** */
+	private static final long serialVersionUID = 710873621129254338L;
+
+	/** */
+	private static final Set<MediaType> SUPPORTED_TYPES =
+            Collections.unmodifiableSet(new HashSet<MediaType>(Arrays.asList(
+                  MediaType.application("pdf")
+            )));
+
+	/* (non-Javadoc)
+	 * @see org.apache.tika.parser.Parser#getSupportedTypes(org.apache.tika.parser.ParseContext)
+	 */
+	@Override
+	public Set<MediaType> getSupportedTypes(ParseContext context) {
+		return SUPPORTED_TYPES;
+	}
+
+	/* (non-Javadoc)
+	 * @see org.apache.tika.parser.Parser#parse(java.io.InputStream, org.xml.sax.ContentHandler, org.apache.tika.metadata.Metadata, org.apache.tika.parser.ParseContext)
+	 */
+	@Override
+	public void parse(InputStream stream, ContentHandler handler,
+			Metadata metadata, ParseContext context) throws IOException,
+			SAXException, TikaException {
+		
+		ValidationResult result = null;
+
+		InputStreamDataSource isds = new InputStreamDataSource(stream);
+		PreflightParser parser = new PreflightParser(isds);
+		try {
+
+		  /* Parse the PDF file with PreflightParser that inherits from the NonSequentialParser.
+		   * Some additional controls are present to check a set of PDF/A requirements. 
+		   * (Stream length consistency, EOL after some Keyword...)
+		   */
+		  parser.parse();
+
+		  /* Once the syntax validation is done, 
+		   * the parser can provide a PreflightDocument 
+		   * (that inherits from PDDocument) 
+		   * This document process the end of PDF/A validation.
+		   */
+		  PreflightDocument document = parser.getPreflightDocument();
+		  document.validate();
+
+		  // Get validation result
+		  result = document.getResult();
+		  document.close();
+
+		} catch (SyntaxValidationException e) {
+		  /* the parse method can throw a SyntaxValidationException 
+		   *if the PDF file can't be parsed.
+		   *
+		   *In this case, the exception contains an instance of ValidationResult
+		   */
+		  result = e.getResult();
+		}
+
+		// display validation result
+		if (result.isValid()) {
+		  System.out.println("The resource is not a valid PDF/A-1b file");
+		} else {
+		  System.out.println("The resource is not valid, error(s) :");
+		  for (ValidationError error : result.getErrorsList()) {
+		    System.out.println(error.getErrorCode() + " : " + error.getDetails());
+		    metadata.add("PDF-A-"+error.getErrorCode(), error.getDetails());
+		  }
+		}
+
+	}
+
+}
