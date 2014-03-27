81981573b8fbdb324643f32c90fc547557252531
Andy Jackson <Andrew.Jackson@bl.uk>
2014-03-27 10:56:16 +0000

    Enabling compression of map outputs.

--- warc-hadoop-indexer/src/main/java/uk/bl/wa/hadoop/indexer/WARCIndexerRunner.java
+++ warc-hadoop-indexer/src/main/java/uk/bl/wa/hadoop/indexer/WARCIndexerRunner.java
@@ -160,6 +160,10 @@ public class WARCIndexerRunner extends Configured implements Tool {
 		conf.setReducerClass( WARCIndexerReducer.class );
 		conf.setOutputFormat( TextOutputFormat.class );
 		conf.set( "map.output.key.field.separator", "" );
+		// Compress the output from the maps, to cut down temp space requirements between map and reduce.
+		conf.setBoolean("mapreduce.map.output.compress", true);
+		// Ensure the JARs we provide take precedence over ones from Hadoop:
+		conf.setBoolean("mapreduce.task.classpath.user.precedence", true);
 
 		conf.setOutputKeyClass( Text.class );
 		conf.setOutputValueClass( Text.class );
