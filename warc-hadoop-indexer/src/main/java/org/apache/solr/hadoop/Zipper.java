17c5d52fb68a2a9999b2f180c0a3fd19e7475005
Andrew Jackson <Andrew.Jackson@bl.uk>
2014-04-10 13:29:59 +0100

    Fixed up the Zipper to skip the outer folder.

--- warc-hadoop-indexer/src/main/java/org/apache/solr/hadoop/Zipper.java
+++ warc-hadoop-indexer/src/main/java/org/apache/solr/hadoop/Zipper.java
@@ -18,47 +18,55 @@ public class Zipper {
 		FileOutputStream fW = null;
 		fW = new FileOutputStream(nameZipFile);
 		zip = new ZipOutputStream(fW);
-		addFolderToZip("", dirName.toString(), zip);
+		for (File f : dirName.listFiles()) {
+			if (f.isDirectory()) {
+				addFolderToZip("", f, zip);
+			} else {
+				addFileToZip("", f, zip, false);
+			}
+		}
 		zip.close();
 		fW.close();
 	}
 
-	private static void addFolderToZip(String path, String srcFolder,
+	private static void addFolderToZip(String path, File srcFolder,
 			ZipOutputStream zip) throws IOException {
-		File folder = new File(srcFolder);
-		if (folder.list().length == 0) {
+		if (srcFolder.list().length == 0) {
 			addFileToZip(path, srcFolder, zip, true);
 		} else {
-			for (String fileName : folder.list()) {
+			for (File f : srcFolder.listFiles()) {
 				if (path.equals("")) {
-					addFileToZip(folder.getName(), srcFolder + "/" + fileName,
+					addFileToZip(srcFolder.getName(), f,
 							zip, false);
 				} else {
-					addFileToZip(path + "/" + folder.getName(), srcFolder + "/"
-							+ fileName, zip, false);
+					addFileToZip(path + "/" + srcFolder.getName(), f, zip,
+							false);
 				}
 			}
 		}
 	}
 
-	private static void addFileToZip(String path, String srcFile,
+	private static void addFileToZip(String path, File folder,
 			ZipOutputStream zip,
 			boolean flag) throws IOException {
-		File folder = new File(srcFile);
 		if (flag) {
 			zip.putNextEntry(new ZipEntry(path + "/" + folder.getName() + "/"));
 		} else {
 			if (folder.isDirectory()) {
-				addFolderToZip(path, srcFile, zip);
+				addFolderToZip(path, folder, zip);
 			} else {
 				byte[] buf = new byte[1024];
 				int len;
-				FileInputStream in = new FileInputStream(srcFile);
+				FileInputStream in = new FileInputStream(folder);
 				zip.putNextEntry(new ZipEntry(path + "/" + folder.getName()));
 				while ((len = in.read(buf)) > 0) {
 					zip.write(buf, 0, len);
 				}
 			}
 		}
+	}
+
+	public static void main(String[] args) throws IOException {
+		Zipper.zipDir(new File("src/test"), new File("temp.zip"));
 	}
 }
