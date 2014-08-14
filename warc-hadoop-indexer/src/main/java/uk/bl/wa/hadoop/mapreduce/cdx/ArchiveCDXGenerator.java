2daa875a4e689e5e55a4e0793230555f6dbd8974
Roger G. Coram <roger.coram@bl.uk>
2014-08-14 14:38:34 +0100

    Added meta-tag to CDX to distinguish collections.

--- warc-hadoop-indexer/src/main/java/uk/bl/wa/hadoop/mapreduce/cdx/ArchiveCDXGenerator.java
+++ warc-hadoop-indexer/src/main/java/uk/bl/wa/hadoop/mapreduce/cdx/ArchiveCDXGenerator.java
@@ -38,104 +38,116 @@ import uk.bl.wa.hadoop.mapreduce.lib.ArchiveToCDXFileInputFormat;
  * 
  */
 
-@SuppressWarnings( "static-access" )
+@SuppressWarnings("static-access")
 public class ArchiveCDXGenerator extends Configured implements Tool {
-	private String inputPath;
-	private String outputPath;
-	private String splitFile;
-	private String cdxFormat;
-	private boolean hdfs;
-	private boolean wait;
-	private int numReducers;
+    private String inputPath;
+    private String outputPath;
+    private String splitFile;
+    private String cdxFormat;
+    private boolean hdfs;
+    private boolean wait;
+    private int numReducers;
+    private String metaTag = "O";
 
-	private void setup( String args[], Configuration conf ) throws ParseException, URISyntaxException {
-		Options options = new Options();
-		options.addOption( "i", true, "input file list" );
-		options.addOption( "o", true, "output directory" );
-		options.addOption( "s", true, "split file" );
-		options.addOption( "c", true, "CDX format" );
-		options.addOption( "h", false, "HDFS refs." );
-		options.addOption( "w", false, "Wait for completion." );
-		options.addOption( "r", true, "Num. Reducers" );
-		options.addOption( "a", true, "ARK identifier lookup" );
-		options.addOption( OptionBuilder.withArgName( "property=value" ).hasArgs(2).withValueSeparator().withDescription( "use value for given property" ).create( "D" ) );
+    private void setup(String args[], Configuration conf)
+	    throws ParseException, URISyntaxException {
+	Options options = new Options();
+	options.addOption("i", true, "input file list");
+	options.addOption("o", true, "output directory");
+	options.addOption("s", true, "split file");
+	options.addOption("c", true, "CDX format");
+	options.addOption("h", false, "HDFS refs.");
+	options.addOption("w", false, "Wait for completion.");
+	options.addOption("r", true, "Num. Reducers");
+	options.addOption("a", true, "ARK identifier lookup");
+	options.addOption("m", true, "Meta-tag character");
+	options.addOption(OptionBuilder.withArgName("property=value")
+		.hasArgs(2).withValueSeparator()
+		.withDescription("use value for given property").create("D"));
 
-		CommandLineParser parser = new PosixParser();
-		CommandLine cmd = parser.parse( options, args );
-		this.inputPath = cmd.getOptionValue( "i" );
-		this.outputPath = cmd.getOptionValue( "o" );
-		this.splitFile = cmd.getOptionValue( "s" );
-		this.cdxFormat = cmd.getOptionValue( "c", " CDX A b a m s k r M V g" );
-		this.hdfs = cmd.hasOption( "h" );
-		this.wait = cmd.hasOption( "w" );
-		this.numReducers = Integer.parseInt( cmd.getOptionValue( "r" ) );
-		if( cmd.hasOption( "a" ) ) {
-			URI lookup = new URI( cmd.getOptionValue( "a" ) );
-			System.out.println( "Adding ARK lookup: " + lookup );
-			DistributedCache.addCacheFile( lookup, conf );
-		}
-		if( inputPath == null || outputPath == null || splitFile == null ) {
-			HelpFormatter formatter = new HelpFormatter();
-			formatter.printHelp( "ArchiveCDXGenerator", options );
-			System.exit( 1 );
-		}
+	CommandLineParser parser = new PosixParser();
+	CommandLine cmd = parser.parse(options, args);
+	this.inputPath = cmd.getOptionValue("i");
+	this.outputPath = cmd.getOptionValue("o");
+	this.splitFile = cmd.getOptionValue("s");
+	this.cdxFormat = cmd.getOptionValue("c", " CDX A b a m s k r M V g");
+	this.hdfs = cmd.hasOption("h");
+	this.wait = cmd.hasOption("w");
+	this.numReducers = Integer.parseInt(cmd.getOptionValue("r"));
+	if (cmd.hasOption("a")) {
+	    URI lookup = new URI(cmd.getOptionValue("a"));
+	    System.out.println("Adding ARK lookup: " + lookup);
+	    DistributedCache.addCacheFile(lookup, conf);
 	}
-
-	private static long getNumMapTasks( Path split, Configuration conf ) throws IOException {
-		FileSystem fs = split.getFileSystem( conf );
-		FSDataInputStream input = fs.open( split );
-		BufferedReader br = new BufferedReader( new InputStreamReader( input ) );
-		long lineCount = 0L;
-		while( br.readLine() != null ) {
-			lineCount++;
-		}
-		input.close();
-		if( lineCount < 3 )
-			return 1;
-		else
-			return( lineCount / 3 );
+	if (inputPath == null || outputPath == null || splitFile == null) {
+	    HelpFormatter formatter = new HelpFormatter();
+	    formatter.printHelp("ArchiveCDXGenerator", options);
+	    System.exit(1);
 	}
-
-	@Override
-	public int run( String[] args ) throws Exception {
-		Job job = new Job( getConf(), "ArchiveCDXGenerator" + "_" + System.currentTimeMillis() );
-		Configuration conf = job.getConfiguration();
-		this.setup( args, conf );
-
-		Path input = new Path( this.inputPath );
-		FileInputFormat.addInputPath( job, input );
-		FileOutputFormat.setOutputPath( job, new Path( this.outputPath ) );
-		job.setInputFormatClass( ArchiveToCDXFileInputFormat.class );
-		job.setOutputFormatClass( TextOutputFormat.class );
-		conf.set( "map.output.key.field.separator", "" );
-		conf.set( "cdx.format", this.cdxFormat );
-		conf.set( "cdx.hdfs", Boolean.toString( this.hdfs ) );
-		conf.set( "mapred.map.tasks.speculative.execution", "false" );
-		conf.set( "mapred.reduce.tasks.speculative.execution", "false" );
-		AlphaPartitioner.setPartitionPath( conf, this.splitFile );
-
-		job.setMapperClass( Mapper.class );
-		job.setMapOutputKeyClass( Text.class );
-		job.setMapOutputValueClass( Text.class );
-		job.setPartitionerClass( AlphaPartitioner.class );
-		job.setReducerClass( Reducer.class );
-		job.setOutputKeyClass( Text.class );
-		job.setOutputValueClass( Text.class );
-		job.setNumReduceTasks( this.numReducers );
-		job.setJarByClass( ArchiveCDXGenerator.class );
-
-		FileSystem fs = input.getFileSystem( conf );
-		FileStatus inputStatus = fs.getFileStatus( input );
-		FileInputFormat.setMaxInputSplitSize( job, inputStatus.getLen() / getNumMapTasks( new Path( this.inputPath ), conf ) );
-		job.submit();
-		if( this.wait )
-			job.waitForCompletion( true );
-		return 0;
+	if (cmd.hasOption("m")) {
+	    metaTag = cmd.getOptionValue("m");
 	}
+    }
 
-	public static void main( String[] args ) throws Exception {
-		ArchiveCDXGenerator cdx = new ArchiveCDXGenerator();
-		int ret = ToolRunner.run( cdx, args );
-		System.exit( ret );
+    private static long getNumMapTasks(Path split, Configuration conf)
+	    throws IOException {
+	FileSystem fs = split.getFileSystem(conf);
+	FSDataInputStream input = fs.open(split);
+	BufferedReader br = new BufferedReader(new InputStreamReader(input));
+	long lineCount = 0L;
+	while (br.readLine() != null) {
+	    lineCount++;
 	}
+	input.close();
+	if (lineCount < 3)
+	    return 1;
+	else
+	    return (lineCount / 3);
+    }
+
+    @Override
+    public int run(String[] args) throws Exception {
+	Job job = new Job(getConf(), "ArchiveCDXGenerator" + "_"
+		+ System.currentTimeMillis());
+	Configuration conf = job.getConfiguration();
+	this.setup(args, conf);
+
+	Path input = new Path(this.inputPath);
+	FileInputFormat.addInputPath(job, input);
+	FileOutputFormat.setOutputPath(job, new Path(this.outputPath));
+	job.setInputFormatClass(ArchiveToCDXFileInputFormat.class);
+	job.setOutputFormatClass(TextOutputFormat.class);
+	conf.set("map.output.key.field.separator", "");
+	conf.set("cdx.format", this.cdxFormat);
+	conf.set("cdx.hdfs", Boolean.toString(this.hdfs));
+	conf.set("cdx.metatag", this.metaTag);
+	conf.set("mapred.map.tasks.speculative.execution", "false");
+	conf.set("mapred.reduce.tasks.speculative.execution", "false");
+	AlphaPartitioner.setPartitionPath(conf, this.splitFile);
+
+	job.setMapperClass(Mapper.class);
+	job.setMapOutputKeyClass(Text.class);
+	job.setMapOutputValueClass(Text.class);
+	job.setPartitionerClass(AlphaPartitioner.class);
+	job.setReducerClass(Reducer.class);
+	job.setOutputKeyClass(Text.class);
+	job.setOutputValueClass(Text.class);
+	job.setNumReduceTasks(this.numReducers);
+	job.setJarByClass(ArchiveCDXGenerator.class);
+
+	FileSystem fs = input.getFileSystem(conf);
+	FileStatus inputStatus = fs.getFileStatus(input);
+	FileInputFormat.setMaxInputSplitSize(job, inputStatus.getLen()
+		/ getNumMapTasks(new Path(this.inputPath), conf));
+	job.submit();
+	if (this.wait)
+	    job.waitForCompletion(true);
+	return 0;
+    }
+
+    public static void main(String[] args) throws Exception {
+	ArchiveCDXGenerator cdx = new ArchiveCDXGenerator();
+	int ret = ToolRunner.run(cdx, args);
+	System.exit(ret);
+    }
 }
--- warc-hadoop-recordreaders/src/main/java/uk/bl/wa/hadoop/mapreduce/lib/DereferencingArchiveToCDXRecordReader.java
+++ warc-hadoop-recordreaders/src/main/java/uk/bl/wa/hadoop/mapreduce/lib/DereferencingArchiveToCDXRecordReader.java
@@ -34,138 +34,170 @@ import org.archive.wayback.resourceindex.cdx.format.CDXFormatException;
 import org.archive.wayback.resourcestore.indexer.ArcIndexer;
 import org.archive.wayback.resourcestore.indexer.WarcIndexer;
 
-public class DereferencingArchiveToCDXRecordReader<Key extends WritableComparable<?>, Value extends Writable> extends RecordReader<Text, Text> {
-	private static final Logger LOGGER = Logger.getLogger( DereferencingArchiveToCDXRecordReader.class.getName() );
-	private LineRecordReader internal = new LineRecordReader();
-	private FSDataInputStream datainputstream;
-	private FileSystem filesystem;
-	private ArchiveReader arcreader;
-	private Iterator<CaptureSearchResult> archiveIterator;
-	private Iterator<String> cdxlines;
-	private WarcIndexer warcIndexer = new WarcIndexer();
-	private ArcIndexer arcIndexer = new ArcIndexer();
-	private Text key;
-	private Text value;
-	private CDXFormat cdxFormat;
-	private boolean hdfs;
-	private HashMap<String, String> warcArkLookup = new HashMap<String, String>();
+public class DereferencingArchiveToCDXRecordReader<Key extends WritableComparable<?>, Value extends Writable>
+	extends RecordReader<Text, Text> {
+    private static final Logger LOGGER = Logger
+	    .getLogger(DereferencingArchiveToCDXRecordReader.class.getName());
+    private LineRecordReader internal = new LineRecordReader();
+    private FSDataInputStream datainputstream;
+    private FileSystem filesystem;
+    private ArchiveReader arcreader;
+    private Iterator<CaptureSearchResult> archiveIterator;
+    private Iterator<String> cdxlines;
+    private WarcIndexer warcIndexer = new WarcIndexer();
+    private ArcIndexer arcIndexer = new ArcIndexer();
+    private Text key;
+    private Text value;
+    private CDXFormat cdxFormat;
+    private boolean hdfs;
+    private String metaTag;
+    private HashMap<String, String> warcArkLookup = new HashMap<String, String>();
 
-	@Override
-	public void initialize( InputSplit split, TaskAttemptContext context ) throws IOException, InterruptedException {
-		Configuration conf = context.getConfiguration();
-		FileSplit fileSplit = ( FileSplit ) split;
-		this.filesystem = fileSplit.getPath().getFileSystem( conf );
-		try {
-			this.cdxFormat = new CDXFormat( conf.get( "cdx.format", " CDX A b a m s k r M V g" ) );
-			this.hdfs = Boolean.parseBoolean( conf.get( "cdx.hdfs", "false" ) );
-		} catch( CDXFormatException e ) {
-			LOGGER.error( "initialize(): " + e.getMessage() );
-		}
-		internal.initialize( split, context );
-		this.getLookup( conf );
+    @Override
+    public void initialize(InputSplit split, TaskAttemptContext context)
+	    throws IOException, InterruptedException {
+	Configuration conf = context.getConfiguration();
+	FileSplit fileSplit = (FileSplit) split;
+	this.filesystem = fileSplit.getPath().getFileSystem(conf);
+	try {
+	    this.cdxFormat = new CDXFormat(conf.get("cdx.format",
+		    " CDX A b a m s k r M V g"));
+	    this.hdfs = Boolean.parseBoolean(conf.get("cdx.hdfs", "false"));
+	} catch (CDXFormatException e) {
+	    LOGGER.error("initialize(): " + e.getMessage());
 	}
+	internal.initialize(split, context);
+	this.getLookup(conf);
+	metaTag = conf.get("cdx.metatag");
+    }
 
-	private void getLookup( Configuration conf ) {
-		try {
-			URI[] uris = DistributedCache.getCacheFiles( conf );
-			if( uris != null ) {
-				for( URI uri : uris ) {
-					FSDataInputStream input = this.filesystem.open( new Path( uri.getPath() ) );
-					BufferedReader reader = new BufferedReader( new InputStreamReader( input ) );
-					String line;
-					String[] values;
-					while( ( line = reader.readLine() ) != null ) {
-						values = line.split( "\\s+" );
-						warcArkLookup.put( values[ 0 ], values[ 1 ] );
-					}
-					System.out.println( "Added " + warcArkLookup.size() + " entries to ARK lookup." );
-				}
-			}
-		} catch( Exception e ) {
-			e.printStackTrace();
+    private void getLookup(Configuration conf) {
+	try {
+	    URI[] uris = DistributedCache.getCacheFiles(conf);
+	    if (uris != null) {
+		for (URI uri : uris) {
+		    FSDataInputStream input = this.filesystem.open(new Path(uri
+			    .getPath()));
+		    BufferedReader reader = new BufferedReader(
+			    new InputStreamReader(input));
+		    String line;
+		    String[] values;
+		    while ((line = reader.readLine()) != null) {
+			values = line.split("\\s+");
+			warcArkLookup.put(values[0], values[1]);
+		    }
+		    System.out.println("Added " + warcArkLookup.size()
+			    + " entries to ARK lookup.");
 		}
+	    }
+	} catch (Exception e) {
+	    e.printStackTrace();
 	}
+    }
 
-	@Override
-	public void close() {
-		if( datainputstream != null ) {
-			try {
-				datainputstream.close();
-			} catch( IOException e ) {
-				System.err.println( "close(): " + e.getMessage() );
-			}
-		}
-		try {
-			internal.close();
-		} catch( IOException e ) {
-			LOGGER.error( "close(): " + e.getMessage() );
-		}
+    @Override
+    public void close() {
+	if (datainputstream != null) {
+	    try {
+		datainputstream.close();
+	    } catch (IOException e) {
+		System.err.println("close(): " + e.getMessage());
+	    }
 	}
-
-	@Override
-	public float getProgress() throws IOException {
-		return internal.getProgress();
+	try {
+	    internal.close();
+	} catch (IOException e) {
+	    LOGGER.error("close(): " + e.getMessage());
 	}
+    }
 
-	@Override
-	public boolean nextKeyValue() {
-		if( this.key == null ) {
-			this.key = new Text();
-		}
-		if( this.value == null ) {
-			this.value = new Text();
-		}
-		String line;
-		while( true ) {
-			try {
-				if( cdxlines != null && cdxlines.hasNext() ) {
-					if( this.hdfs ) {
-						line = hdfsPath( cdxlines.next(), this.internal.getCurrentValue().toString() );
-					} else {
-						line = cdxlines.next();
-					}
-					this.key.set( line );
-					this.value.set( line );
-					return true;
-				} else {
-					if( this.internal.nextKeyValue() ) {
-						Path path = new Path( this.internal.getCurrentValue().toString() );
-						datainputstream = this.filesystem.open( path );
-						arcreader = ArchiveReaderFactory.get( path.getName(), datainputstream, true );
-						arcreader.setStrict( false );
-						if( path.getName().matches( "^.+\\.warc(\\.gz)?$" ) ) {
-							archiveIterator = warcIndexer.iterator( ( WARCReader ) arcreader );
-						} else {
-							archiveIterator = arcIndexer.iterator( ( ARCReader ) arcreader );
-						}
-						cdxlines = SearchResultToCDXFormatAdapter.adapt( archiveIterator, cdxFormat );
-					} else {
-						return false;
-					}
-				}
-			} catch( Exception e ) {
-				LOGGER.error( "nextKeyValue: " + e.getMessage() );
-			}
-		}
+    @Override
+    public float getProgress() throws IOException {
+	return internal.getProgress();
+    }
+
+    @Override
+    public boolean nextKeyValue() {
+	if (this.key == null) {
+	    this.key = new Text();
 	}
-
-	@Override
-	public Text getCurrentKey() throws IOException, InterruptedException {
-		return this.key;
+	if (this.value == null) {
+	    this.value = new Text();
 	}
-
-	@Override
-	public Text getCurrentValue() throws IOException, InterruptedException {
-		return this.value;
-	}
-
-	private String hdfsPath( String cdx, String path ) throws URISyntaxException {
-		String[] fields = cdx.split( " " );
-		if( warcArkLookup.size() != 0 ) {
-			fields[ 9 ] = warcArkLookup.get( fields[ 9 ] ) + "#" + fields[ 9 ];
+	String line;
+	while (true) {
+	    try {
+		if (cdxlines != null && cdxlines.hasNext()) {
+		    if (this.hdfs) {
+			line = hdfsPath(cdxlines.next(), this.internal
+				.getCurrentValue().toString());
+		    } else {
+			line = cdxlines.next();
+		    }
+		    line = setMetaTag(line);
+		    this.key.set(line);
+		    this.value.set(line);
+		    return true;
 		} else {
-			fields[ 9 ] = new URI( path ).getPath() + "?user.name=hadoop&bogus=.warc.gz";
+		    if (this.internal.nextKeyValue()) {
+			Path path = new Path(this.internal.getCurrentValue()
+				.toString());
+			datainputstream = this.filesystem.open(path);
+			arcreader = ArchiveReaderFactory.get(path.getName(),
+				datainputstream, true);
+			arcreader.setStrict(false);
+			if (path.getName().matches("^.+\\.warc(\\.gz)?$")) {
+			    archiveIterator = warcIndexer
+				    .iterator((WARCReader) arcreader);
+			} else {
+			    archiveIterator = arcIndexer
+				    .iterator((ARCReader) arcreader);
+			}
+			cdxlines = SearchResultToCDXFormatAdapter.adapt(
+				archiveIterator, cdxFormat);
+		    } else {
+			return false;
+		    }
 		}
-		return StringUtils.join( fields, " " );
+	    } catch (Exception e) {
+		LOGGER.error("nextKeyValue: " + e.getMessage());
+	    }
 	}
+    }
+
+    @Override
+    public Text getCurrentKey() throws IOException, InterruptedException {
+	return this.key;
+    }
+
+    @Override
+    public Text getCurrentValue() throws IOException, InterruptedException {
+	return this.value;
+    }
+
+    private String hdfsPath(String cdx, String path) throws URISyntaxException {
+	String[] fields = cdx.split(" ");
+	if (warcArkLookup.size() != 0) {
+	    fields[9] = warcArkLookup.get(fields[9]) + "#" + fields[9];
+	} else {
+	    fields[9] = new URI(path).getPath()
+		    + "?user.name=hadoop&bogus=.warc.gz";
+	}
+	return StringUtils.join(fields, " ");
+    }
+
+    /**
+     * We use the "meta tags" field to identify the CDX's licence.
+     * @param cdx
+     * @return
+     */
+    private String setMetaTag(String cdx) {
+	String[] fields = cdx.split(" ");
+	if (fields[7].equals("-")) {
+	    fields[7] = metaTag;
+	} else {
+	    fields[7] = metaTag + fields[7];
+	}
+	return StringUtils.join(fields, " ");
+    }
 }
