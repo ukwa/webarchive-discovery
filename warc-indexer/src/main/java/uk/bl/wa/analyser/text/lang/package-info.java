2c551efa1f1791dd102c5ce4df215c981b57a90e
Toke Eskildsen <te@ekot.dk>
2015-02-13 14:35:51 +0100

    Added packe info

--- warc-indexer/src/main/java/uk/bl/wa/analyser/text/lang/package-info.java
+++ warc-indexer/src/main/java/uk/bl/wa/analyser/text/lang/package-info.java
@@ -6,5 +6,13 @@
  * shows that the speed of text detection is a bit more than doubled.
  * </p><p>
  * The changes should be upstreamed to Tika and are only in warc-indexer until that happens.
+ * </p><p>
+ * Caveat: The code is experimental has not been properly tested!
+ * </p><p>
+ * Future improvements: LanguageProfile#distance is still the hotspot in the code.
+ * As all operations are on Strings, the sequential iterations of the lists are really
+ * random access throughout the Java heap. Switching to 2 large char-arrays with c-style
+ * 0-delimited entities would raise the probability of the relevant data being in Level 2
+ * cache and likely improve speed significantly.
  */
 package uk.bl.wa.analyser.text.lang;
