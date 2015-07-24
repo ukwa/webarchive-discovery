056d5203583724ead63c83d5b2638d1693eade1c
Andrew Jackson <Andrew.Jackson@bl.uk>
2015-07-24 09:24:06 +0100

    Add a dedicated test resource.

--- warc-hadoop-indexer/src/test/java/uk/bl/wa/hadoop/mapreduce/mdx/MDXSequenceIteratorTest.java
+++ warc-hadoop-indexer/src/test/java/uk/bl/wa/hadoop/mapreduce/mdx/MDXSequenceIteratorTest.java
@@ -1,11 +1,17 @@
 package uk.bl.wa.hadoop.mapreduce.mdx;
 
+import static org.junit.Assert.assertEquals;
+
+import java.io.File;
 import java.util.Iterator;
 
 import org.junit.Before;
 import org.junit.Test;
 
 public class MDXSequenceIteratorTest {
+
+	private File mdxWarcBothSeq = new File(
+			"src/test/resources/mdx-seq/mdx-warc-both.seq");
 
 	@Before
 	public void setUp() throws Exception {
@@ -15,14 +21,17 @@ public class MDXSequenceIteratorTest {
 	public void testMDXOutput() throws Exception {
 
 		// Check contents of the output:
-		Iterator<MDX> i = new MDXSequenceIterator(
-				WARCMDXGeneratorIntegrationTest.outputSeq);
+		Iterator<MDX> i = new MDXSequenceIterator(mdxWarcBothSeq);
 		MDX mdx;
+		int counter = 0;
 		while (i.hasNext()) {
 			mdx = i.next();
 			System.out.println("Key is: " + mdx.getHash() + " record_type: "
 					+ mdx.getRecordType() + " SURT: " + mdx.getUrlAsSURT());
+			counter++;
 		}
+		System.out.println("Total records: " + counter);
+		assertEquals(114, counter);
 	}
 
 }
