f53be73017917d7bab11018507b35229f38a3111
Andrew Jackson <Andrew.Jackson@bl.uk>
2020-05-16 21:35:42 +0100

    Use normal TextOutputFormat and remove class with confusing name.

--- warc-hadoop-recordreaders/src/main/java/uk/bl/wa/hadoop/mapreduce/cdx/ArchiveCDXGenerator.java
+++ warc-hadoop-recordreaders/src/main/java/uk/bl/wa/hadoop/mapreduce/cdx/ArchiveCDXGenerator.java
@@ -53,12 +53,12 @@ import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
 import org.apache.hadoop.mapreduce.lib.partition.TotalOrderPartitioner;
 import org.apache.hadoop.util.Tool;
 import org.apache.hadoop.util.ToolRunner;
 import org.archive.hadoop.mapreduce.AlphaPartitioner;
 
-import uk.bl.wa.hadoop.mapreduce.io.TextOutputFormat;
 import uk.bl.wa.hadoop.mapreduce.lib.ArchiveToCDXFileInputFormat;
 import uk.bl.wa.hadoop.mapreduce.lib.DereferencingArchiveToCDXRecordReader;
 
