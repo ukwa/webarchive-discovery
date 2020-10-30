ed7125e62688ef17d0b851c2877cab20d1760896
Toke Eskildsen <te@ekot.dk>
2020-10-30 16:25:54 +0100

    Add check for 0-length content

--- warc-indexer/src/main/java/uk/bl/wa/util/InputStreamUtils.java
+++ warc-indexer/src/main/java/uk/bl/wa/util/InputStreamUtils.java
@@ -10,12 +10,12 @@ package uk.bl.wa.util;
  * it under the terms of the GNU General Public License as
  * published by the Free Software Foundation, either version 2 of the
  * License, or (at your option) any later version.
- *
+ * 
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- *
+ * 
  * You should have received a copy of the GNU General Public
  * License along with this program.  If not, see
  * <http://www.gnu.org/licenses/gpl-2.0.html>.
@@ -105,7 +105,8 @@ public class InputStreamUtils {
             throws IOException {
         HashedInputStream hash = new HashedInputStream(url, expectedHash, checkHash, input, length);
         InputStream stream = CachedInputStreamFactory.cacheContent(
-                maybeDecompress(maybeDechunk(hash, chunkHint), compressionHint),
+                // Don't try to de-chunk or de-compress if length is 0
+                length == 0 ? hash : maybeDecompress(maybeDechunk(hash, chunkHint), compressionHint),
                 length, false, true, inMemoryThreshold, onDiskThreshold);
         return new HashIS(stream, hash);
     }
